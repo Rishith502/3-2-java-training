f11547a0c2b40b87fc537627e1bf412bdcb765eb
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2025-12-10 21:16:09 +0530

    Add files via upload

---
+++ Day1_Datatypes.java
@@ -0,0 +1,13 @@
+public class Day1_Datatypes {
+    public static void main(String[] args) {
+        byte a = 125;
+        short b = 2342;
+        int c = 502;
+        long d = 9845435;
+        float e = 55;
+        double f = 542.090;
+        boolean g = true;
+        char h = 'R';
+        System.out.println(h);
+    }
+}
