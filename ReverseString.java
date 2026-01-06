fd5f8637f5e45ece981600eb8242a273df00e33b
Rishith Pothanna Gari <rishithpothannagari25@gmail.com>
2026-01-06 10:40:41 +0530

    message

--- ReverseString.java
+++ ReverseString.java
@@ -2,9 +2,8 @@ public class ReverseString {
     public static void main(String[] args) {
         
         String str = "Rishith";
-        for(str=0;srt<=str.length;str++){
-            str[]--;
-        }
+
+
     
     }
 }
